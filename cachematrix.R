7c72bc4b7ee15ef3dbb14ef69505c9f349fd31ac
DataScienceRick <erikwhite90@gmail.com>
2017-05-13 13:49:37 -0400

    Edited stub files to accomplish objectives of the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The intention of these functions is to outline the role of lexical
+## scoping in the context of cached data. These functions allow for
+## computations to be stored in the memory of an object. This allows
+## us to avoid repetive calculations, by instead referencing the
+## value of the previous solution in the event that it was already
+## determined.
 
-## Write a short comment describing this function
+## This function creates a special object that closely resembles a 
+## matrix. It allows for the inverse of a matrix to be cached in the
+## memory of the object - so that it does not have to be calculated
+## every time it is retrieved.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inv) i <<- inv
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function determines the inverse of the matrix component of the 
+## 'makeCache' object that is passed to it. It checks to see if the inverse
+## of the matrix has already been computed and cached. If it has been, then
+## it will simply retrieve this cached value, foregoing any computations. If
+## the inverse of the matrix has not been cached, then the function will compute
+## the value, and cache it into the memory of the 'cacheMatrix' object.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if (!is.null(i)){
+                message ("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setinverse(i)
+        i
 }
